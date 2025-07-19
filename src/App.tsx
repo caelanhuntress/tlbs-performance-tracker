efcee367b2c6ee4db7b329677be4b51c0c0c4c13
Caelan Huntress <caelan@caelanhuntress.com>
2025-07-19 16:27:40 +1200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Calendar from "./pages/Calendar";
-import Data from "./pages/Data";
-import Dashboard from "./pages/Dashboard";
-import NotFound from "./pages/NotFound";
-import Auth from "./pages/Auth";
+import { EntriesProvider } from "./context/EntriesContext";
+// ...other imports
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Calendar />} />
-            <Route path="data" element={<Data />} />
-            <Route path="dashboard" element={<Dashboard />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <EntriesProvider>
+      {/* ...your routing and pages/components */}
+    </EntriesProvider>
+  );
+}
 
 export default App;
